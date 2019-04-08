b3819cd61ae2657afdfb7af64af8f4175d3553e3
MBearo <295290708@qq.com>
2019-04-08 21:12:04 +0800

    UPG:test get pi temp

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const serve = require('koa-static');
 const Koa = require('koa');
+const fs=require('fs')
 const app = new Koa();
 
-app.use(serve(__dirname + '/static'));
+app.use(async (ctx,next)=>{
+  serve(__dirname + '/static')
+  await next()
+});
+
+app.use(async ctx=>{
+  let temp=`I'm raspberry pi !`
+  try {
+    temp= (fs.readFileSync('/sys/class/thermal/thermal_zone0/temp','utf8'))/1000+'℃'
+  } catch (error) {
+   console.log(error)
+  }
+  ctx.body=temp
+})
 
 app.listen(3000);
 
