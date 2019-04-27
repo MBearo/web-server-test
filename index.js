fb968635b09f53a2fa7fb955a11bff228769f602
MBearo <295290708@qq.com>
2019-04-27 13:08:17 +0800

    FIX:修复无法实用的bug

--- index.js
+++ index.js
@@ -5,19 +5,8 @@ const app = new Koa();
 
 app.use(async (ctx,next)=>{
   serve(__dirname + '/static')
-  await next()
 });
 
-app.use(async ctx=>{
-  let temp=`I'm raspberry pi !`
-  try {
-    temp= (fs.readFileSync('/sys/class/thermal/thermal_zone0/temp','utf8'))/1000+'℃'
-  } catch (error) {
-   console.log(error)
-  }
-  ctx.body=temp
-})
+app.listen(80);
 
-app.listen(3000);
-
-console.log('listening on port 3000');
+console.log('listening on port 80');
